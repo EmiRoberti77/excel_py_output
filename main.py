46b0e0e2099c5ec281799c437dbbaed7fd51ff9d
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-04-27 11:09:15 +0100

    reading variables from config file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from report.excel_output import ExcelReportOutput
+from dotenv import load_dotenv
+import os
+load_dotenv()
+TEMPLATE = os.getenv("TEMPLATE_INPUT_PATH")
+OUTPUT = os.getenv("REPORT_OUTPUT_PATH")
+INPUT = os.getenv("REPORT_INPUT_PATH")
 
-excel_output = ExcelReportOutput('template/template.xlsx', 'output.xlsx')
+excel_output = ExcelReportOutput(TEMPLATE, OUTPUT)
 excel_output.output()
 
 
--- report/excel_output.py
+++ report/excel_output.py
@@ -1,6 +1,17 @@
 from openpyxl import Workbook
+from dotenv import load_dotenv
 import openpyxl
+import os
+load_dotenv()
 
+MIN_ROW= int(os.getenv('MIN_ROW'))
+MAX_ROW=int(os.getenv('MAX_ROW'))
+MIN_COL=int(os.getenv('MIN_COL'))
+MAX_COL=int(os.getenv('MAX_COL'))
+print(MIN_ROW)
+print(MAX_ROW)
+print(MIN_COL)
+print(MAX_COL)
 """
 class to produce excel output
 """
@@ -19,12 +30,12 @@ class ExcelReportOutput:
       template = openpyxl.load_workbook(self.template_path)
       print('template has been opened')
       ws = template.active
-      for row in ws.iter_rows(min_row=1, max_row=10, min_col=1, max_col=5):
+      for row in ws.iter_rows(min_row=MIN_ROW, max_row=MAX_ROW, min_col=MIN_COL, max_col=MAX_COL):
         for cell in row:
           if cell.value is not None:
             print(f"Data found in {cell.coordinate}: {cell.value}")
-    except Exception:
-      print(Exception)
+    except Exception as e: 
+      print(e)
       print('failed to load file')
 
 
