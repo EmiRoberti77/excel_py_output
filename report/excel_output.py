6c22456619f0102ba275cf2310e66d94cce11825
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-04-27 11:51:18 +0100

    values extracted from switch statement

--- report/excel_output.py
+++ report/excel_output.py
@@ -1,13 +1,17 @@
 from openpyxl import Workbook
 from dotenv import load_dotenv
 import openpyxl
+import random
 import os
 load_dotenv()
 
-MIN_ROW= int(os.getenv('MIN_ROW'))
+MIN_ROW=int(os.getenv('MIN_ROW'))
 MAX_ROW=int(os.getenv('MAX_ROW'))
 MIN_COL=int(os.getenv('MIN_COL'))
 MAX_COL=int(os.getenv('MAX_COL'))
+_SEP = ":"
+
+
 """
 class to produce excel output
 """
@@ -43,9 +47,39 @@ class ExcelReportOutput:
   def output(self):
     cells_dict = self.read_template()
     print(cells_dict)
-    print(f"template:{self.template_path}")
-    print(f"output:{self.output_path}")
     wb = Workbook()
     ws = wb.active
-    ws['A1'] = 'Origin CMM output'
+    for cell in cells_dict:
+      print(cell,_SEP,cells_dict[cell])
+      val = self.getValueFor(cells_dict[cell])
+      ws[cell] = val
+      print(f"setting {cell}={val}")
+
     wb.save(self.output_path)
+  
+  """
+  extract value for the template cell
+  """
+  def getValueFor(self, value):
+    return self.switch(value)
+  
+
+  """
+  find the correct token and replace it with its new computed value
+  """
+  def switch(self,value)->any:
+    print('value=>', value)
+    match value:
+      case "{o_t_1}":
+        return self.computeResult()
+      case "{o_t_2}":
+        return self.computeResult()
+      case "{o_t_3}":
+        return self.computeResult()
+      case _:
+       return 0
+      
+
+  def computeResult(self):
+    return random.randint(200, 4000)
+
