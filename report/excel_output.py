db2fc24479d72fced1516584d606a36a3d4870ae
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-04-27 11:23:54 +0100

    read_template returns dictionary of all target cells to update

--- report/excel_output.py
+++ report/excel_output.py
@@ -8,10 +8,6 @@ MIN_ROW= int(os.getenv('MIN_ROW'))
 MAX_ROW=int(os.getenv('MAX_ROW'))
 MIN_COL=int(os.getenv('MIN_COL'))
 MAX_COL=int(os.getenv('MAX_COL'))
-print(MIN_ROW)
-print(MAX_ROW)
-print(MIN_COL)
-print(MAX_COL)
 """
 class to produce excel output
 """
@@ -25,25 +21,28 @@ class ExcelReportOutput:
   """
   extract all values from the template
   """
-  def read_template(self):
+  def read_template(self)->any:
+    cells_dict = dict()
     try:
       template = openpyxl.load_workbook(self.template_path)
       print('template has been opened')
       ws = template.active
       for row in ws.iter_rows(min_row=MIN_ROW, max_row=MAX_ROW, min_col=MIN_COL, max_col=MAX_COL):
         for cell in row:
-          if cell.value is not None:
-            print(f"Data found in {cell.coordinate}: {cell.value}")
+          if cell.value is not None:         
+            cells_dict[cell.coordinate]=cell.value   
     except Exception as e: 
       print(e)
       print('failed to load file')
+    return cells_dict
 
 
   """
   produce excel report output
   """
   def output(self):
-    self.read_template()
+    cells_dict = self.read_template()
+    print(cells_dict)
     print(f"template:{self.template_path}")
     print(f"output:{self.output_path}")
     wb = Workbook()
