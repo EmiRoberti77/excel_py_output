e5f5a967bd5c4f5704e16efbfd71b8e5cd6ca190
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-05-02 12:47:17 +0100

    option to pass template as system args

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from report.excel_output import ExcelReportOutput
 from dotenv import load_dotenv
 import os
+import sys
 load_dotenv()
-TEMPLATE = os.getenv("TEMPLATE_INPUT_PATH")
+TEMPLATE_1 = os.getenv("TEMPLATE_INPUT_PATH_1")
+TEMPLATE_2 = os.getenv("TEMPLATE_INPUT_PATH_2")
 OUTPUT = os.getenv("REPORT_OUTPUT_PATH")
 INPUT = os.getenv("REPORT_INPUT_PATH")
 
-excel_output = ExcelReportOutput(TEMPLATE, OUTPUT, INPUT)
+program = sys.argv[0]
+template = sys.argv[1]
+if template is None:
+  print("missing template option")
+else:
+  if template == "1":
+    template = TEMPLATE_1
+  else: 
+    template = TEMPLATE_2
+
+  excel_output = ExcelReportOutput(template, OUTPUT, INPUT)
 excel_output.output()
 
 
